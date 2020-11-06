6d160d701b9c1d65570f5ab179b44bd7942b864a
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-11-06 10:01:27 +0100

    Person med instansmetoder

---
+++ src/Person.java
@@ -0,0 +1,87 @@
+import java.util.Calendar;
+
+/**
+ * Created by Mahmud Al Hakim
+ * Date: 2020-11-06
+ * Time: 09:09
+ * Project: KVALIT20-OOP-Lektion20
+ * Copyright: MIT
+ */
+public class Person {
+
+  // Instansvariabler
+  String firstName;
+  String lastName;
+  String dateOfBirth;
+  double height;
+  double weight;
+
+  // Instansmetoder
+
+  /**
+   * En instansmetod som returnerar hela namnet
+   *
+   * @return "förnamn efternamn"
+   */
+  public String getName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  /**
+   * En instansmetod som beräknar ålder
+   *
+   * @return ålder
+   */
+  public int getAge() {
+    String calendar = Calendar.getInstance().getTime().toString();
+    String thisYearString = calendar.substring(calendar.lastIndexOf(' ') + 1);
+    int thisYear = Integer.parseInt(thisYearString);
+
+    String yearString = this.dateOfBirth.substring(0, 4);
+    int year = Integer.parseInt(yearString);
+
+    return thisYear - year;
+  }
+
+  /**
+   * En instansmetod som beräknar BMI
+   * @return BMI
+   */
+  public double getBMI(){
+    // return this.weight / (this.height * this.height);
+    // Eller utan this
+    return weight / (height * height);
+  }
+
+  /**
+   * En instansmetod som beräknar viktklassen
+   * @return viktklassen
+   */
+  public String getBMICategory(){
+
+    // double BMI = this.getBMI();
+    // Eller utan this
+    double BMI = getBMI();
+
+    String category;
+
+    if(BMI < 18.5)
+      category = "Undervikt";
+    else if (BMI <= 25)
+      category = "Normalvikt";
+    else
+      category = "Övervikt";
+
+    return category;
+
+  }
+
+
+  @Override
+  public String toString() {
+    return "Namn: " + getName() +
+        "\nÅlder: " + getAge() + " år" +
+        "\nBMI: " + getBMI() +
+        "\nViktklass: " + getBMICategory() + "\n";
+  }
+}
---
+++ src/PersonDemo.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Date: 2020-11-06
+ * Time: 09:19
+ * Project: KVALIT20-OOP-Lektion20
+ * Copyright: MIT
+ */
+public class PersonDemo {
+
+  public static void main(String[] args) {
+    Person p1 = new Person();
+    p1.firstName = "Mahmud";
+    p1.lastName = "Al Hakim";
+    p1.dateOfBirth = "19730202";
+    p1.height = 1.70;
+    p1.weight = 85;
+    System.out.println(p1.getName());
+    System.out.println("Ålder: " + p1.getAge() + " år.");
+    System.out.printf("BMI: %.2f \n" , p1.getBMI());
+    System.out.println("Viktklassen: " + p1.getBMICategory());
+
+    Person p2 = new Person();
+    p2.firstName = "Yasmin";
+    p2.lastName = "Al Hakim";
+    p2.dateOfBirth = "19750606";
+    p2.height = 1.68;
+    p2.weight = 60;
+    System.out.println(p2.getName());
+    System.out.println("Ålder: " + p2.getAge() + " år." );
+    System.out.printf("BMI: %.2f \n" , p2.getBMI());
+    System.out.println("Viktklassen: " + p2.getBMICategory());
+    System.out.println();
+
+    // Skriv ut ett objekt
+    System.out.println(p1);
+    System.out.println(p2);
+
+
+  }
+
+}
